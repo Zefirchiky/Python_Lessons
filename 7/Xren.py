65f235e6d3100e2b23033d742494d330489c5bd9
Mudruk Artem <74060222+Zefirchiky@users.noreply.github.com>
2021-10-13 20:00:09 +0300

    cghl;

---
+++ 7/Xren.py
@@ -0,0 +1,6 @@
+n = int(input("n="))
+k = 0
+for i in range(1,n+1):
+    k+=1/2**i
+print("S =", k)
+    
---
+++ 7/Преобраз.py
@@ -0,0 +1,6 @@
+a = [1, 2, 3, 4, 5]
+k = 0
+for i in a:
+    a[k] = float(i)
+    k+=1
+print (a)
